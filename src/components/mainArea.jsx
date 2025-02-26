d8d309d40b6f891558d481f6fbe92eec2d93fdc7
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-02-25 21:09:49 -0700

    fetched pokemon list

--- src/components/mainArea.jsx
+++ src/components/mainArea.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 
 const MainArea = () => {
+
+
+  //pokemon list fetch
+  async function pokemonList() {
+    const pokemonListURL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    try{
+      const listResponse = await fetch (pokemonListURL);
+      if(!listResponse.ok){
+        throw new error(`Response for pokemon list: ${listResponse.status}`)
+      }
+    const pokemonListData = await listResponse.json();
+    console.log(pokemonListData)
+    } 
+    
+    catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  pokemonList()
   return (
     <div>
       <section className='pokedex-left'>
