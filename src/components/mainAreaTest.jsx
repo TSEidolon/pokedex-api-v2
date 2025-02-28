5ede5989ad7ad4396f84006e307669b54e5ce91a
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-02-27 23:54:20 -0700

    edited styling

--- src/components/mainAreaTest.jsx
+++ src/components/mainAreaTest.jsx
@@ -44,44 +44,46 @@ const MainAreaTest = () => {
         <input className='search-box' type="text" placeholder='Search...' value={searchTerm} onChange={event => setSearchTerm(event.target.value)}
         />
       </section>
-      <section className='pokedex-left'>
-        <div className="h-[100px] w-[100px] border-2 border-green-600">Top</div>
-        <div className="h-[100px] w-[100px] border-2 border-green-600">Bottom</div>
-      </section>
-      <section className="pokedex-middle h-[400px] w-[350px] border-2 border-black">
-        {
-          selectedPokemon && (
-            <div>
-              <h2 className="">{selectedPokemon.name}</h2>
-              <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name}  />
-              <p>Height: {selectedPokemon.height}</p>
-              <p>Weight: {selectedPokemon.weight}</p>
+      <main className="flex justify-center items-center gap-5">
+        <section className='pokedex-left flex flex-col gap-5'>
+          <div className="h-[100px] w-[100px] border-2 border-green-600">Top</div>
+          <div className="h-[100px] w-[100px] border-2 border-green-600">Bottom</div>
+        </section>
+        <section className="pokedex-middle h-[400px] w-[350px] border-2 border-black">
+          {
+            selectedPokemon && (
+              <div>
+                <h2 className="">{selectedPokemon.name}</h2>
+                <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name}  />
+                <p>Height: {selectedPokemon.height}</p>
+                <p>Weight: {selectedPokemon.weight}</p>
 
-              {
-                selectedPokemon.stats.map((stat, index) => (
-                  <div>
-                    <p> {stat.stat.name}: {stat.base_stat} </p>
-                  </div>
-                ))
-              }
-            </div>
-          )
-        }
-      </section>
-
-      <section className='pokedex-right border-2 border-black h-[500px] overflow-y-auto'>
-        <ul>
-          {filteredPokemonList.map((pokemon)=> {
-            return(
-              <li key={pokemon.url}>
-                <a onClick={()=> showPokemon(pokemon.url)} href="#" >{pokemon.name}</a>
-              </li>
+                {
+                  selectedPokemon.stats.map((stat, index) => (
+                    <div>
+                      <p> {stat.stat.name}: {stat.base_stat} </p>
+                    </div>
+                  ))
+                }
+              </div>
             )
-          })
-
           }
-        </ul>
-      </section>
+        </section>
+
+        <section className='pokedex-right border-2 border-black h-[500px] overflow-y-auto'>
+          <ul>
+            {filteredPokemonList.map((pokemon)=> {
+              return(
+                <li key={pokemon.url}>
+                  <a onClick={()=> showPokemon(pokemon.url)} href="#" >{pokemon.name}</a>
+                </li>
+              )
+            })
+
+            }
+          </ul>
+        </section>
+      </main>
     </div>
   )
 }
