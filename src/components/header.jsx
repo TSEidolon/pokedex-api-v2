d6ea41aa6a41ee8e8deae14731c2a0226fab2fcd
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-03-07 09:24:53 -0700

    pre-build commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,9 @@ import pokeball from "../assets/pokeball.png"
 
 const Header = () => {
   return (
-    <div className='flex justify-center text-4xl py-2 lg:py-4 select-none gap-2' >
+    <div className='flex justify-center items-center text-4xl py-2 lg:py-4 select-none gap-2' >
       <p className="font-semibold">Pokedex API</p> 
-      <img className="w-[35px] h-[35px] " src={pokeball} alt="different pokeballs" />
+      <img className="size-[30px] " src={pokeball} alt="different pokeballs" />
     </div>
   )
 }
